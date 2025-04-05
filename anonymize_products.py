d55c1992fac048c44310f320d1e138136c3ca479
Simon Jerabek <simonjerabek275@gmail.com>
2025-04-05 19:14:31 +0000

    Produkty anonymizované a uložené

--- anonymize_products.py
+++ anonymize_products.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('datasets/Shoptet/Products_temp.csv')
+# Načítanie pôvodného CSV súboru
+df = pd.read_csv('datasets/Shoptet/Products_part1.csv')
 
 # Anonymizované stĺpce
 cols_to_anonymize = ['purchasePrice', 'relativeMargin', 'absoluteMargin']
@@ -8,6 +9,12 @@ for col in cols_to_anonymize:
     if col in df.columns:
         df[col] = '***'
 
+# Oprava ceny - prevod na eurá (pre hodnoty 485 na 4.85)
+df['price'] = df['price'] / 100
+df['standardPrice'] = df['standardPrice'] / 100
+df['purchasePrice'] = df['purchasePrice'] / 100
+
+# Uloženie anonymizovaného a opraveného súboru
 df.to_csv('datasets/Shoptet/Products_anonymized.csv', index=False)
-print(" Produkty anonymizované a uložené do: datasets/Shoptet/Products_anonymized.csv")
+print("Produkty anonymizované a ceny opravené a uložené do: datasets/Shoptet/Products_anonymized.csv")
 
